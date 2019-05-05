d3baddd9ac735b6e0952c884930c7e87967ea8c0
hitungan <kevinalim@rocketmail.com>
2019-05-05 16:40:18 +0700

    Tambah alert

--- app/src/main/java/com/android/unikaja/MainActivity.java
+++ app/src/main/java/com/android/unikaja/MainActivity.java
@@ -1,9 +1,12 @@
 package com.android.unikaja;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -32,13 +35,62 @@ public class MainActivity extends AppCompatActivity {
         r = findViewById(R.id.hasil);
         clt = findViewById(R.id.button);
 
+        tbh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                krng.setChecked(false);
+                bg.setChecked(false);
+                kl.setChecked(false);
+            }
+        });
+
+        krng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bg.setChecked(false);
+                tbh.setChecked(false);
+                kl.setChecked(false);
+            }
+        });
+
+        kl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                krng.setChecked(false);
+                tbh.setChecked(false);
+                bg.setChecked(false);
+            }
+        });
+
+        bg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                krng.setChecked(false);
+                tbh.setChecked(false);
+                kl.setChecked(false);
+            }
+        });
+
         clt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int a, b;
+                int a=0, b=0;
                 int hasil = 0;
-                a = Integer.parseInt(num1.getText().toString());
-                b = Integer.parseInt(num2.getText().toString());
+                try {
+                    a = Integer.parseInt(num1.getText().toString());
+                    b = Integer.parseInt(num2.getText().toString());
+                } catch (Exception e) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertBuilder.setTitle("Masukin angka dulu");
+                    alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    alertBuilder.create().show();
+                    return;
+                }
 
                 if (tbh.isChecked()) {
                     hasil = a + b;
@@ -47,7 +99,22 @@ public class MainActivity extends AppCompatActivity {
                 } else if (kl.isChecked()){
                     hasil = a*b;
                 } else if (bg.isChecked()){
+                    if (b == 0) {
+                        r.setText("Tak Terhingga");
+                        return;
+                    }
                     hasil = a/b;
+                } else {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertBuilder.setTitle("Pilih Dulu");
+                    alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    alertBuilder.create().show();
+                    return;
                 }
 
                 r.setText(String.valueOf(hasil));
